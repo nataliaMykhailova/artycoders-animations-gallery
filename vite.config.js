d911f92b3cf3de8c69bfa6b24f3a6d82200b0e4a
nataliaMykhailova <natalia.mykhailova.95@gmail.com>
2025-06-13 13:52:25 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
+    base: './',
+    build: {
+        outDir: 'docs',
+        assetsDir: 'assets',
+    },
     plugins: [
         viteStaticCopy({
             targets: [
